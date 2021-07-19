64489bce654603e0b7cbd6d9e90032df44ab19f4
Steve Jaehyeok Kim <stevejh.kim0322@gmail.com>
2021-07-19 19:08:53 +0900

    Error in loading model fixed

--- eval.py
+++ eval.py
@@ -1,56 +1,29 @@
 from data import *
-from rouge import Rouge
 
 import torch
 import torch.nn as nn
 
-from util.bleu import idx_to_word, get_bleu
-
-
-
-def eval(model, iterator):
-    epoch_loss = 0
-    batch_bleu = []
-    batch_rouge = []
-    rouge = Rouge()
-    with torch.no_grad():
-        for i, batch in enumerate(iterator):
-            src = batch.src
-            trg = batch.trg
-            output = model(src, trg[:, :-1])
-            output_reshape = output.contiguous().view(-1, output.shape[-1])
-            trg = trg[:, 1:].contiguous().view(-1)
-
-            total_bleu = []
-            total_rouge = []
-            for j in range(batch_size):
-                try:
-                    trg_words = idx_to_word(batch.trg[j], loader.target.vocab)
-                    output_words = output[j].max(dim=1)[1]
-                    output_words = idx_to_word(output_words, loader.target.vocab)
-                    bleu = get_bleu(hypotheses=output_words.split(), reference=trg_words.split())
-                    total_bleu.append(bleu)
-
-                    rouge_score = rouge.get_scores(output_words, trg_words)
-                    total_rouge.append(rouge_score[0]['rouge-l']['r'])  # getting recall value
-                except:
-                    pass
-
-            total_bleu = sum(total_bleu) / len(total_bleu)
-            batch_bleu.append(total_bleu)
-
-            total_rouge = sum(total_rouge) / len(total_rouge)
-            batch_rouge.append(total_rouge)
-
-    batch_bleu = sum(batch_bleu) / len(batch_bleu)
-    batch_rouge = sum(batch_rouge) / len(batch_rouge)
-
-    return epoch_loss / len(iterator), batch_bleu, batch_rouge
+from train import evaluate
+from models.model.transformer import Transformer
 
 
 def compute_metrics():
-    model = torch.load('./saved/model-saved.pt', map_location=torch.device('cpu'))
-    _, bleu, rouge = eval(model, valid_iter)
+    model = Transformer(src_pad_idx=src_pad_idx,
+                        trg_pad_idx=trg_pad_idx,
+                        trg_sos_idx=trg_sos_idx,
+                        d_model=d_model,
+                        enc_voc_size=enc_voc_size,
+                        dec_voc_size=dec_voc_size,
+                        max_len=max_len,
+                        ffn_hidden=ffn_hidden,
+                        n_head=n_heads,
+                        n_layers=n_layers,
+                        drop_prob=0.00,
+                        device=device).to(device)
+
+    criterion = nn.CrossEntropyLoss(ignore_index=src_pad_idx)
+    model.load_state_dict(torch.load("./saved/model-saved.pt", map_location=torch.device('cpu')))
+    _, bleu, rouge = evaluate(model, valid_iter, criterion)
 
     print(f'\tBLEU Score: {bleu:.3f}')
     print(f'\tRouge Recall: {rouge:.3f}')
